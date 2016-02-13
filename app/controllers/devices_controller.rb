a16b30f89034b0c64ea1f87da1453ca4ae88f9a0
Mickael Sergent <mickael.sergent@free.fr>
2016-02-13 14:37:14 +0100

    insert device edit page

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,12 +1,40 @@
 class DevicesController < ApplicationController
+  before_filter :load_device , only: [:show,:edit,:update, :destroy]
   
   def index
     @devices = Device.all
   end
   
   def show
-    @device = Device.find(params[:id])
+    if (@device != nil) then
     @sensors = @device.sensors
+    end
   end
   
+  def edit
+    
+  end
+  
+  def update
+    if (@device.update(device_params)) then
+      flash[:info] = "device correctly updated"
+      redirect_to device_url(@device)
+    else
+      flash[:error] = "device update failed"
+      render :edit
+    end  end
+  
+  def destroy
+    
+  end
+  
+private
+
+  def load_device
+    @device = Device.find(params[:id])
+  end
+
+  def device_params
+    params.require(:device).permit(:address, :name, :location, :follow)
+  end
 end
