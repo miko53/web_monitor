9f37d8570330bfa056d35763445a300f97439ea8
Mickael Sergent <mickael.sergent@free.fr>
2020-01-04 12:58:49 +0100

    add page management on create/update/destroy schedule command

--- app/controllers/range_commands_controller.rb
+++ app/controllers/range_commands_controller.rb
@@ -4,16 +4,20 @@ class RangeCommandsController < ApplicationController
   
   def index
     @actuator_commands = RangeCommand.all.order(:name).page(params[:page]).per_page(10)
+    @page = params[:page]
   end
   
   def new
     @actuator_command = RangeCommand.new
+    @page = params[:page]
   end
   
   def edit
+    @page = params[:page]
   end
   
   def create
+    @page = params[:page]
     @actuator_command = RangeCommand.new(range_command_params)
     actuator_command_id = params.require(:actuators_range_commands).permit(:actuator_ids => [] )
     #p actuator_command_id 
@@ -22,7 +26,7 @@ class RangeCommandsController < ApplicationController
       flash[:info] = "time slot created successfully"
       @actuator_command.actuator_ids = actuator_command_id[:actuator_ids]
       @actuator_command.save
-      redirect_to range_commands_path
+      redirect_to range_commands_path(:page => @page)
     else
       flash[:error] = "report saved failed"
       render :new
@@ -30,17 +34,19 @@ class RangeCommandsController < ApplicationController
   end
   
   def destroy
+    @page = params[:page]
     @actuator_command.destroy 
     flash[:info] = "time slot removed"
-    redirect_to range_commands_path
+    redirect_to range_commands_path(:page => @page)
   end
   
   def update
+    @page = params[:page]
     actuator_command_id = params.require(:actuators_range_commands).permit(:actuator_ids => [] )
     @actuator_command.actuator_ids = actuator_command_id[:actuator_ids]
     if (@actuator_command.update(range_command_params)) then
         flash[:info] = "time slot correctly updated"
-        redirect_to range_commands_path
+        redirect_to range_commands_path(:page => @page)
     else
        flash[:error] = "update failed"
         render :edit
