db2d6d033ef26206330096cbea0ce43650a2b936
Mickael Sergent <mickael.sergent@free.fr>
2020-09-13 08:57:44 +0200

    test with function of update module here

--- app/helpers/electrical_retrieval.rb
+++ app/helpers/electrical_retrieval.rb
@@ -1,6 +1,6 @@
 require 'linky_meter'
 require 'date'
-#require 'update_helper'
+require 'update_helper'
 #require 'byebug'
 
 class ElectricalRetrieval
@@ -10,10 +10,7 @@ class ElectricalRetrieval
     password = ENV['LINKY_PASSWORD']
     authentication_cookie = ENV['LINKY_COOKIE_INTERNAL_AUTH_ID']
 
-    linky = LinkyMeter.new()
-    #p username
-    #p password
-    #p authentication_cookie
+    linky = LinkyMeter.new(true)
     linky.connect(username, password, authentication_cookie)
 
     date_yesterday = DateTime.now - 1 # retrieve date of yesterday
@@ -52,10 +49,10 @@ class ElectricalRetrieval
       items_list << item
     end
     
-    d = UpdateHelper::get_device(linky_device)
-    s = UpdateHelper::create_sensor(d, 0, "ElectricalMeter")
+    d = get_device(linky_device)
+    s = create_sensor(d, 0, "ElectricalMeter")
     if (d.follow) then
-      UpdateHelper::insert_sample_datas(s, items_list)
+      insert_sample_datas(s, items_list)
     end
     
     
@@ -74,9 +71,82 @@ class ElectricalRetrieval
       items_list << item
     end
     
-    s = UpdateHelper::create_sensor(d, 1, "ElectricalConsumption")
+    s = create_sensor(d, 1, "ElectricalConsumption")
     if (d.follow) then
-      UpdateHelper::insert_sample_datas(s, items_list)
+      insert_sample_datas(s, items_list)
+    end
+  end
+
+  
+  def self.get_device(device_adress)
+    #p device_data
+    device = Device.find_by_address(device_adress)
+    if (device == nil) then
+      ActiveRecord::Base.transaction do
+        device = Device.create(address: device_adress, follow: false)
+      end
+    end
+    
+    return device
+  end
+  
+  def self.create_sensor(device, sensor_order, sensor_type)
+    s = device.sensors.find_by_order(sensor_order)
+    if (s == nil) then
+      s = create_sensor_1(device, sensor_order, sensor_type)
+    end
+    return s
+  end
+
+  def self.create_sensor_1(device,  sensor_order, sensor_type)
+    #p "d --> #{d}"
+    ioType = :sensor
+    if (ioType == :sensor) then
+      s = device.sensors.create(order: sensor_order,  sensor_type: sensor_type)
+    else
+      s = nil
+#     s = device.actuators.create(order: d["id"], actuator_type: type)
+    end
+    return s
+  end
+  
+  def self.insert_sample_datas(sensor, datas)
+    case (sensor.sensor_type)
+      when "Temperature"
+        db = TemperatureDatum
+      when "Humidity"
+        db = HumidityDatum
+      when "Voltage"
+        db = VoltageDatum
+      when "Pressure"
+        db = PressureDatum
+      when "ElectricalMeter"
+        db = ElectricalPowerDatum
+      when "ElectricalConsumption"
+        db = ElectricalConsumptionDatum
+      else
+        raise "DB doesn't exist"
+    end
+
+    #to avoid to insert data in double, erase previous ones before (if exists)
+    ActiveRecord::Base.transaction do
+      datas.each do |d|
+        previous = db.find_by(dateTime: d['date'], sensor_id: sensor.id)
+        if (previous != nil) then
+          previous.destroy
+        end
+      end
+    end
+    
+    ActiveRecord::Base.transaction do
+      #byebug
+      datas.each do |d|
+        t = db.new
+        t.value = d['value']
+        t.sensor_id = sensor.id
+        t.dateTime = d['date']
+        t.save
+      end
     end
     
   end
