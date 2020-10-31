6f48d98b63586a0dec5f2f5dc9654809763444b9
Mickael Sergent <mickael.sergent@free.fr>
2020-10-31 12:43:51 +0100

    update help for retrieved electrical data to add debug information and avoid to insert nan data into DB

--- app/helpers/electrical_retrieval.rb
+++ app/helpers/electrical_retrieval.rb
@@ -9,17 +9,14 @@ class ElectricalRetrieval
     username = ENV['LINKY_USERNAME']
     password = ENV['LINKY_PASSWORD']
     authentication_cookie = ENV['LINKY_COOKIE_INTERNAL_AUTH_ID']
-
-    linky = LinkyMeter.new()
-    #p username
-    #p password
-    #p authentication_cookie
+    
+    linky = LinkyMeter.new(false)
     linky.connect(username, password, authentication_cookie)
 
-    date_yesterday = DateTime.now - 1 # retrieve date of yesterday
+    date_yesterday = DateTime.now - 3 # retrieve date of yesterday
     
     result = linky.get(date_yesterday, DateTime.now,  LinkyMeter::BY_HOUR)
-    
+    p result    
 #     result = {"1"=>{"CONS"=>{"labels"=>["2020-08-15T00:30:00.000+0200", "2020-08-15T01:00:00.000+0200", "2020-08-15T01:30:00.000+0200", "2020-08-15T02:00:00.000+0200", 
 #                                         "2020-08-15T02:30:00.000+0200", "2020-08-15T03:00:00.000+0200", "2020-08-15T03:30:00.000+0200", "2020-08-15T04:00:00.000+0200", 
 #                                         "2020-08-15T04:30:00.000+0200", "2020-08-15T05:00:00.000+0200", "2020-08-15T05:30:00.000+0200", "2020-08-15T06:00:00.000+0200", 
@@ -36,6 +33,13 @@ class ElectricalRetrieval
 #                                       0.19, 0.19, 0.128, 0.096, 0.096, 0.106, 0.198, 0.188, 0.188, 0.132, 0.096, 0.096, 0.098, 0.196, 0.19, 0.186, 0.136, 0.096, 0.094, 0.096, 0.198, 
 #                                       0.188, 0.188, 0.14, 0.096, 0.096, 0.098, 0.198, 0.192, 2.99, 4.096, 4.206, 3.602, 3.604, 3.562, 3.596, 3.966, 1.604, 1.2, 1.318, 1.544, 2.824, 
 #                                       0.416, 2.092, 1.7, 0.97, 0.236], "mesuresPasEnum"=>"PT30M", "grandeurMetier"=>"CONS", "grandeurPhysique"=>"PA", "unite"=>"W"}}}
+
+    p "check is NaN"
+    isNan = false
+    if (result['1']['CONS']['data'][0] == "NaN") then
+      p "it is NaN --> quit"
+      isNan = true
+    end
     
     linky_device = ENV['LINKY_DEVICE_NAME']
     #linky_device = 'web@linky_home'
@@ -44,6 +48,7 @@ class ElectricalRetrieval
     for i in 0..result['1']['CONS']['labels'].count-1 
       item_date = result['1']['CONS']['labels'][i]
       item_data = result['1']['CONS']['data'][i]
+      
       item = Hash.new
       item['date'] = DateTime.parse(item_date)
       item['value'] = item_data * 1000 #set in W not kW
@@ -52,20 +57,23 @@ class ElectricalRetrieval
       items_list << item
     end
     
-    d = UpdateHelper::get_device(linky_device)
-    s = UpdateHelper::create_sensor(d, 0, "ElectricalMeter")
-    if (d.follow) then
-      UpdateHelper::insert_sample_datas(s, items_list)
+    if (isNan == false) then
+      d = get_device(linky_device)
+      s = create_sensor(d, 0, "ElectricalMeter")
+      if (d.follow) then
+        insert_sample_datas(s, items_list)
+      end
     end
     
+    result = linky.get(date_yesterday, DateTime.now,  LinkyMeter::BY_DAY)
+#   result = {"1"=>{"CONS"=>{"aggregats"=>{"JOUR"=>{"labels"=>["Vendredi 04/09/20"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-09-04T00:00:00.000+0200",  "dateFin"=>"2020-09-04T00:00:00.000+0200"}], "datas"=>[17.82]}, "SEMAINE"=>{"labels"=>["du 31/08/20 au 06/09/20"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-08-31T00:00:00.000+0200", "dateFin"=>"2020-09-06T23:59:59.000+0200"}], "datas"=>[17.82]}, "MOIS"=>{"labels"=>["Septembre 2020"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-09-01T00:00:00.000+0200", "dateFin"=>"2020-09-30T23:59:59.000+0200"}], "datas"=>[17.82]}, "ANNEE"=>{"labels"=>["2020"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-01-01T00:00:00.000+0100", "dateFin"=>"2020-12-31T23:59:59.000+0100"}], "datas"=>[17.82]}}, "grandeurMetier"=>"CONS", "grandeurPhysique"=>"EA", "unite"=>"Wh"}}}
     
-     result = linky.get(date_yesterday, DateTime.now,  LinkyMeter::BY_DAY)
-#    result = {"1"=>{"CONS"=>{"aggregats"=>{"JOUR"=>{"labels"=>["Vendredi 04/09/20"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-09-04T00:00:00.000+0200",  "dateFin"=>"2020-09-04T00:00:00.000+0200"}], "datas"=>[17.82]}, "SEMAINE"=>{"labels"=>["du 31/08/20 au 06/09/20"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-08-31T00:00:00.000+0200", "dateFin"=>"2020-09-06T23:59:59.000+0200"}], "datas"=>[17.82]}, "MOIS"=>{"labels"=>["Septembre 2020"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-09-01T00:00:00.000+0200", "dateFin"=>"2020-09-30T23:59:59.000+0200"}], "datas"=>[17.82]}, "ANNEE"=>{"labels"=>["2020"], "periodes"=>[{"grandeurPhysiqueEnum"=>"EA", "dateDebut"=>"2020-01-01T00:00:00.000+0100", "dateFin"=>"2020-12-31T23:59:59.000+0100"}], "datas"=>[17.82]}}, "grandeurMetier"=>"CONS", "grandeurPhysique"=>"EA", "unite"=>"Wh"}}}
-    
+    p result
     items_list = Array.new
     for i in 0..result['1']['CONS']['aggregats']['JOUR']['periodes'].count-1 
       item_date = result['1']['CONS']['aggregats']['JOUR']['periodes'][i]['dateDebut']
       item_data = result['1']['CONS']['aggregats']['JOUR']['datas'][i]
+      
       item = Hash.new
       item['date'] = DateTime.parse(item_date)
       item['value'] = item_data * 1000 #set in Wh not kWh
@@ -73,10 +81,87 @@ class ElectricalRetrieval
       #p item['value']
       items_list << item
     end
+   
+    if (isNan == false) then 
+      s = create_sensor(d, 1, "ElectricalConsumption")
+      if (d.follow) then
+        insert_sample_datas(s, items_list)
+      end
+    end
+  end
+
+  
+  def self.get_device(device_adress)
+    #p device_data
+    device = Device.find_by_address(device_adress)
+    if (device == nil) then
+      ActiveRecord::Base.transaction do
+        device = Device.create(address: device_adress, follow: false)
+      end
+    end
     
-    s = UpdateHelper::create_sensor(d, 1, "ElectricalConsumption")
-    if (d.follow) then
-      UpdateHelper::insert_sample_datas(s, items_list)
+    return device
+  end
+  
+  def self.create_sensor(device, sensor_order, sensor_type)
+    s = device.sensors.find_by_order(sensor_order)
+    if (s == nil) then
+      s = create_sensor_1(device, sensor_order, sensor_type)
+    end
+    return s
+  end
+
+  def self.create_sensor_1(device,  sensor_order, sensor_type)
+    #p "d --> #{d}"
+    ioType = :sensor
+    if (ioType == :sensor) then
+      s = device.sensors.create(order: sensor_order,  sensor_type: sensor_type)
+    else
+      s = nil
+#     s = device.actuators.create(order: d["id"], actuator_type: type)
+    end
+    return s
+  end
+  
+  def self.insert_sample_datas(sensor, datas)
+    case (sensor.sensor_type)
+      when "Temperature"
+        db = TemperatureDatum
+      when "Humidity"
+        db = HumidityDatum
+      when "Voltage"
+        db = VoltageDatum
+      when "Pressure"
+        db = PressureDatum
+      when "ElectricalMeter"
+        db = ElectricalPowerDatum
+      when "ElectricalConsumption"
+        db = ElectricalConsumptionDatum
+      else
+        raise "DB doesn't exist"
+    end
+
+    #to avoid to insert data in double, erase previous ones before (if exists)
+    ActiveRecord::Base.transaction do
+      datas.each do |d|
+        previous = db.find_by(dateTime: d['date'], sensor_id: sensor.id)
+        if (previous != nil) then
+          previous.destroy
+          #p "delete #{previous.dateTime}"
+        end
+      end
+    end
+    
+    ActiveRecord::Base.transaction do
+      #byebug
+      datas.each do |d|
+        t = db.new
+        t.value = d['value']
+        t.sensor_id = sensor.id
+        t.dateTime = d['date']
+        t.save
+	#p "add #{t.dateTime}"
+      end
     end
     
   end
